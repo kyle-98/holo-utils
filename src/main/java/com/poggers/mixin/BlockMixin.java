bd281268ce8f3b0e3adb3187610b38451750becb
kyle <kforker1@gmail.com>
2025-02-07 20:09:06 -0500

    fixed xray rendering

--- src/main/java/com/poggers/mixin/BlockMixin.java
+++ src/main/java/com/poggers/mixin/BlockMixin.java
@@ -2,9 +2,7 @@ package com.poggers.mixin;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,23 +15,14 @@ import me.shedaniel.autoconfig.AutoConfig;
 
 @Mixin(value = Block.class)
 public class BlockMixin {
-    // @Inject(at = @At("HEAD"), method = "shouldDrawSide(" + "Lnet/minecraft/block/BlockState;" + 
-    //         "Lnet/minecraft/world/BlockView;" + 
-    //         "Lnet/minecraft/util/math/BlockPos;" +
-    //         "Lnet/minecraft/util/math/Direction;" + 
-    //         "Lnet/minecraft/util/math/BlockPos;" + 
-    //         ")Z",
-    //         cancellable = true)
-    // private static void shouldDrawSide(BlockState state, BlockView reader, BlockPos pos, Direction face, BlockPos blockPos, CallbackInfoReturnable<Boolean> ci) {
-    //     ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-    //     if(XrayUtils.getXrayState()){
-    //         if (config.xraySettings.getXrayBlocks().contains(state.getBlock())) {
-    //             ci.setReturnValue(true);
-    //         } else {
-    //             ci.setReturnValue(false);
-    //         }
-    //     }
-    // }
+        if (XrayUtils.getXrayState()) {
+            if (config.xraySettings.getXrayBlocks().contains(state.getBlock())) {
+                ci.setReturnValue(true);
+            } else {
+                ci.setReturnValue(false);
+            }
+        }
+    }
 
     private BlockMixin() {
     }
