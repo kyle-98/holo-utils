f583c55a0e63adc5b3b5cb456d0c3027767506ea
kyle <kforker1@gmail.com>
2025-12-29 20:55:00 -0500

    Updated Gradle to 9.2.1 and mod to 1.21.11

--- src/main/java/com/poggers/mixin/FogRendererMixin.java
+++ src/main/java/com/poggers/mixin/FogRendererMixin.java
@@ -1,57 +1,84 @@
 package com.poggers.mixin;
 
+import java.util.List;
+
+import org.joml.Vector4f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.poggers.config.holoutils.ModConfig;
 import com.poggers.config.holoutils.ModConfig.FogRemoval;
 
 import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraft.block.enums.CameraSubmersionType;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.render.fog.FogData;
 import net.minecraft.client.render.fog.FogModifier;
 import net.minecraft.client.render.fog.FogRenderer;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 @Mixin(FogRenderer.class)
 public abstract class FogRendererMixin {
-    @WrapOperation(
-        method = "applyFog(Lnet/minecraft/client/render/Camera;IZLnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/render/fog/FogModifier;applyStartEndModifier(Lnet/minecraft/client/render/fog/FogData;Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/client/world/ClientWorld;FLnet/minecraft/client/render/RenderTickCounter;)V"
-        )
-    )
-    public void applyFogOverride( FogModifier fogModifier, FogData fogData, Entity entity, BlockPos blockPos, ClientWorld clientWorld, float viewDistance, RenderTickCounter renderTickCounter, Operation<Void> originalOperation ) {
-        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-        MinecraftClient client = MinecraftClient.getInstance();
-        ClientWorld world = client.world;
-        viewDistance /= 2;
 
+    @Shadow
+    @Final
+    private static List<FogModifier> FOG_MODIFIERS;
+
+    @Inject(
+        method = "applyFog(Lnet/minecraft/client/render/Camera;ILnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;", 
+        at = @At(value = "FIELD", 
+            target = "Lnet/minecraft/client/render/fog/FogData;renderDistanceEnd:F", 
+            ordinal = 0, 
+            shift = At.Shift.AFTER
+        ), 
+        locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void onApplyFog(
+        Camera camera, 
+        int renderDistance, 
+        RenderTickCounter renderTickCounter, 
+        float viewDistance, 
+        ClientWorld world, 
+        CallbackInfoReturnable<Vector4f> cir, 
+        float g, 
+        Vector4f vector4f, 
+        float h, 
+        CameraSubmersionType cameraSubmersionType, 
+        Entity entity, 
+        FogData fogData
+    ) {
+        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
         boolean removeFog = config.visualSettings.getAllFogState() == FogRemoval.EVERYWHERE
                 || (config.visualSettings.getAllFogState() == FogRemoval.NETHER_ONLY
                     && world != null && world.getRegistryKey() == World.NETHER);
 
         if (removeFog) {
-            fogData.environmentalStart = Float.MAX_VALUE;
-            fogData.environmentalEnd = Float.MAX_VALUE;
-            fogData.skyEnd = viewDistance;
-            fogData.cloudEnd = client.options.getCloudRenderDistance().getValue() * 16;
-        } else {
-            originalOperation.call(fogModifier, fogData, entity, blockPos, clientWorld, viewDistance, renderTickCounter);
+            for (int i = 0; i < FOG_MODIFIERS.size(); ++i) {
+                if (FOG_MODIFIERS.get(i).shouldApply(cameraSubmersionType, entity)) {
+                    fogData.environmentalStart = Float.MAX_VALUE;
+                    fogData.environmentalEnd = Float.MAX_VALUE;
+                    fogData.renderDistanceStart = Float.MAX_VALUE;
+                    fogData.renderDistanceEnd = Float.MAX_VALUE;
+                    fogData.skyEnd = viewDistance;
+                    fogData.cloudEnd = MinecraftClient.getInstance().options.getCloudRenderDistance().getValue() * 16f;
+                }
+            }
         }
     }
 
     @ModifyConstant(
-        method = "applyFog(Lnet/minecraft/client/render/Camera;IZLnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;", 
+        method = "applyFog(Lnet/minecraft/client/render/Camera;ILnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;", 
         constant = @Constant(intValue = 16)
     )
     private int disableDistanceFog(int originalValue) {
@@ -62,6 +89,7 @@ public abstract class FogRendererMixin {
         boolean removeFog = config.visualSettings.getAllFogState() == FogRemoval.EVERYWHERE
                 || (config.visualSettings.getAllFogState() == FogRemoval.NETHER_ONLY
                     && world != null && world.getRegistryKey() == World.NETHER);
+
         if (removeFog) {
             originalValue *= 2;
         }
