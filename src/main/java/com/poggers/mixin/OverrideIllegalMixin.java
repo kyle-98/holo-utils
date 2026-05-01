f1091d669fe0c6b6af58065b2935dca0a5a78d28
kyle <kforker1@gmail.com>
2026-04-30 21:25:03 -0400

    Added to class tweaker for referencing text and functionalized return values
    - Added text class to the class tweaker to remove final/shadow text values
    - Placed reusable code into a function for checking if gamma option
    - Fixed an issue with the config not properly updating the full bright state.

--- src/main/java/com/poggers/mixin/OverrideIllegalMixin.java
+++ src/main/java/com/poggers/mixin/OverrideIllegalMixin.java
@@ -5,10 +5,12 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,23 +21,19 @@ import java.util.Optional;
 
 @Mixin(SimpleOption.class)
 public class OverrideIllegalMixin<T> {
-    @Shadow
-    T value;
-
-    @Shadow
-    @Final
-    Text text;
+    @Unique
+    private boolean isGammaOption(){
+        Text text = ((SimpleOption<?>)(Object)this).text;
+        return text.getContent() instanceof TranslatableTextContent translatable && "options.gamma".equals(translatable.getKey());
+    }
 
     @Inject(method = "getCodec", at = @At("HEAD"), cancellable = true)
     private void returnFakeCodec(CallbackInfoReturnable<Codec<Double>> info) {
-        if (text.getString().equals(I18n.translate("options.gamma"))) {
-            info.setReturnValue(Codec.DOUBLE);
-        }
+        if (isGammaOption()) info.setReturnValue(Codec.DOUBLE);
     }
 
     @WrapOperation(method = "setValue", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/SimpleOption$Callbacks;validate(Ljava/lang/Object;)Ljava/util/Optional;"))
-    private Optional<T> cancelValidation(SimpleOption.Callbacks instance, T t, Operation<Optional<T>> original) {
-        return text.getString().equals(I18n.translate("options.gamma")) ? Optional.of(t) : original.call(instance, t);
+    private Optional<T> cancelValidation(SimpleOption.Callbacks<T> instance, T t, Operation<Optional<T>> original) {
+        return isGammaOption() ? Optional.of(t) : original.call(instance, t);
     }
-
 }
